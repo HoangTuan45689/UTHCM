61a0df79bf0e639a640a3ba958035355dbc05f55
HoangTuan45689 <hoangxuantuanqb@gmail.com>
2017-05-29 13:19:03 +0700

    Add files via upload

---
+++ Hinh_vuong_dac_1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=1, j=1, n;
+    printf("Nhap vao chi so n: ");
+    scanf("%d", &n);
+    if (n%2==0)
+    {printf("Gia tri cua n phai la mot so le!\n");
+    getch(); }
+    else printf("Gia tri dep day ban :v\n");
+    while (i<=n)
+    {
+          while (j<=n)
+          {
+                printf("* ");
+                j++;
+          }
+          j=1;
+    printf("\n");
+    i++;
+    }
+    getch();
+}
